4052fd1c05b3450d1805bf19cb2374c263319c60
2755802910@qq.com <2755802910@qq.com>
2025-10-27 10:45:37 +0800

    接口好像不能放attribute

--- IExternalOrderService.cs
+++ IExternalOrderService.cs
@@ -7,16 +7,9 @@ namespace ExternalOrderService
 
     public interface IExternalOrderService
     {
-        [Time]
         Task<ApiResponse<bool>> InsertAsync(ExternalOrderDTO dto);
-
-        [Time]
         Task<ApiResponse<ExternalOrderDTO>> GetAsync(string fromPlatform, string Tid);
-
-        [Time]
         Task<ApiResponse<bool>> UpdateAsync(ExternalOrderDTO dto);
-
-        [Time]
         Task<ApiResponse<bool>> DeleteAsync(DeleteRequest request);
 
 
